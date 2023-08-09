237465dbc5b05492215fea9cdb1406ecab859c9b
luris123 <anssi.rauhala1@gmail.com>
2023-08-09 05:19:09 +0300

    aamu viiden ratkasut

--- app/map.tsx
+++ app/map.tsx
@@ -27,7 +27,8 @@ export default function Map() {
   const [longitude, setLongitude] = useState(25.467207074816663);
   const [zoom, setZoom] = useState(13);
   const [bars, setBars] = useState<Bar[]>([]);
-  const [results, setResults] = useState<google.maps.places.PlaceResult[]>([]);
+  const [moreBars, setMoreBars] = useState<Bar[]>([]);
+  const [evenMoreBars, setEvenMoreBars] = useState<Bar[]>([]);
   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=65.01150495824623,25.467207074816663&radius=3000&type=bar&key=${apikey}`;
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
@@ -40,19 +41,43 @@ export default function Map() {
     [latitude, longitude]
   );
 
+  const allBars = [...bars, ...moreBars, ...evenMoreBars];
+
   useEffect(() => {
     const getBars = async () => {
       try {
-        const response = await axios.get(proxyurl + url);
+        let response = await axios.get(proxyurl + url);
         console.log(response.data);
         setBars(response.data.results);
+        if ("next_page_token" in response.data) {
+          //wait for 5 seoconds
+          await new Promise((resolve) => setTimeout(resolve, 5000));
+          let next_page_token = response.data.next_page_token;
+          let response2 = await axios.get(
+            proxyurl +
+              `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=65.01150495824623,25.467207074816663&radius=3000&type=bar&key=${apikey}&pagetoken=${next_page_token}`
+          );
+          console.log(response2.data);
+          setMoreBars(response2.data.results);
+
+          if ("next_page_token" in response2.data) {
+            await new Promise((resolve) => setTimeout(resolve, 5000));
+            let next_page_token = response2.data.next_page_token;
+            let response3 = await axios.get(
+              proxyurl +
+                `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=65.01150495824623,25.467207074816663&radius=3000&type=bar&key=${apikey}&pagetoken=${next_page_token}`
+            );
+            console.log(response3.data);
+            setEvenMoreBars(response3.data.results);
+          }
+        }
       } catch (error) {
         console.log(error);
       }
     };
 
     getBars();
-  }, [url]);
+  }, [apikey, url]);
 
   const showCenter = () => {
     setLatitude(65.01150495824623);
@@ -61,10 +86,11 @@ export default function Map() {
   };
 
   const randomBar = () => {
-    const random = Math.floor(Math.random() * bars.length);
-    setLatitude(bars[random].geometry.location.lat);
-    setLongitude(bars[random].geometry.location.lng);    
+    const random = Math.floor(Math.random() * allBars.length);
+    setLatitude(allBars[random].geometry.location.lat);
+    setLongitude(allBars[random].geometry.location.lng);
     setZoom(18);
+    console.log(allBars);
   };
 
   if (!isLoaded) return <div>Loading... and sniping</div>;
